2a7c6f907db440e5dc1d3a0d4905d373c6697dbd
Federico Bello <fe.debello13@gmail.com>
2024-12-16 16:04:42 -0300

    research: city boundries dataset

--- research/interact_geospatial.py
+++ research/interact_geospatial.py
@@ -18,6 +18,7 @@ ee.Initialize(project="unicef-geospatial")
 METRICS = Literal["frequency", "duration", "severity", "extreme_high_temp"]
 DECADES = Literal["1960s", "1970s", "1980s", "1990s", "2000s", "2010s", "2020s"]
 COUNTRY_BOUNDRIES_DATASET = "USDOS/LSIB_SIMPLE/2017"
+ADMIN_LEVEL_1_BOUNDRIES_DATASET = "WM/geoLab/geoBoundaries/600/ADM1"
 REDUCERS = Literal["mean", "max", "min"]
 
 
@@ -80,10 +81,46 @@ def get_heatwave_metric_for_country(
     return round(stats.getInfo()["b1"], 3)
 
 
+@tool
+def get_heatwave_metric_for_admin_level_1(
+    metric: METRICS,
+    decade: DECADES,
+    admin_level_1_name: str,
+    reducer: REDUCERS = "mean",
+) -> dict:
+    """Get the value of a heatwave metric for a specific city and decade.
+
+    Args:
+        metric: One of 'frequency', 'duration', 'severity', 'extreme_high_temp'
+        decade: One of '1960s', '1970s', '1980s', '1990s', '2000s', '2010s', '2020s'
+        admin_level_1_name: Name of the admin level 1, state, province, etc
+        reducer: The reducer to use ('mean', 'max', 'min', etc). Defaults to 'mean'
+
+    Returns:
+        The value of the heatwave metric for the specified country and decade.
+    """
+    heatwave_tiff = ee.Image(
+        f"projects/unicef-geospatial/assets/heatwaves/{metric}/average_heatwaves_{metric}_{decade}_proj_COG"
+    )
+    admin_level_1_boundries = ee.FeatureCollection(ADMIN_LEVEL_1_BOUNDRIES_DATASET)
+
+    admin_level_1_boundries = admin_level_1_boundries.filter(
+        ee.Filter.eq("shapeName", admin_level_1_name)
+    )
+    admin_level_1_heatwave = heatwave_tiff.clip(admin_level_1_boundries)
+    stats = admin_level_1_heatwave.reduceRegion(
+        reducer=getattr(ee.Reducer, reducer)(),
+        geometry=admin_level_1_boundries.geometry(),
+        scale=1000,
+        maxPixels=1e13,
+    )
+    return round(stats.getInfo()["b1"], 3)
+
+
 # %%
 # Initialize LLM and create agent
 llm = ChatCohere(temperature=0.0)
-tools = [get_heatwave_metric_for_country]
+tools = [get_heatwave_metric_for_country, get_heatwave_metric_for_admin_level_1]
 
 system_prompt = """You are an expert in analyzing heatwave data across different countries and time periods. 
 
@@ -116,10 +153,9 @@ inputs = {
     "messages": [
         {
             "role": "user",
-            "content": "What was the average duration of heatwaves in Uy in the 1990s?",
+            "content": "What was the average duration of heatwaves in Buenos Aires in the 1990s?",
         }
     ]
 }
-res = graph.invoke(inputs)
 print_stream(graph.stream(inputs, stream_mode="values"))
 # %%
