267c0d2fbfdc8006caf0c4d8c53cf2e241474fcf
Federico Bello <fe.debello13@gmail.com>
2025-01-17 16:00:57 -0300

    fix: demographic tool

--- unicef_geospatial/geospatial/demographic/tools.py
+++ unicef_geospatial/geospatial/demographic/tools.py
@@ -1,28 +1,44 @@
-from ee.featurecollection import FeatureCollection
+import ee
 from ee.filter import Filter
 from ee.imagecollection import ImageCollection
 from ee.reducer import Reducer
 from langchain.tools import tool
+from logging_config import get_logger
 from utils.constants import DEMOGRAPHIC_BAND, DEMOGRAPHIC_DATASET
 from utils.types import AGE_GROUPS, SEXES
 
 
 @tool
 def get_population_in_zone(
-    zone_vector: FeatureCollection,
+    path_to_vector_data: str,
     age_group: AGE_GROUPS = "Total Population",
     sex: SEXES = "b",
 ) -> float:
     """Calculate population count within a specified geographic zone.
 
     Args:
+        path_to_vector_data: Path to the geometry to clip the demographic data
         age_group: Age group to analyze.
         sex: Sex to analyze.
-        zone_vector: Earth Engine FeatureCollection defining the geographic zone to analyze
 
     Returns:
         float: Total population count in the zone (in millions)
     """
+    # Convert the dictionary to FeatureCollection
+
+    try:
+        logger = get_logger(__name__)
+        with open(path_to_vector_data, "r") as f:
+            logger.info(f"Going to load vector data from {path_to_vector_data}")
+            json_value = eval(f.read())
+            zone_vector = ee.deserializer.fromJSON(json_value)
+
+        zone_vector = zone_vector.getInfo()
+        zone_vector = ee.FeatureCollection(zone_vector)
+    except Exception as e:
+        logger.error(f"Error in vector conversion: {str(e)}")
+        raise e
+
     demographic = ImageCollection(DEMOGRAPHIC_DATASET)
     demographic_image = (
         demographic.filter(Filter.eq("Age_Group", age_group))
@@ -30,6 +46,7 @@ def get_population_in_zone(
         .first()
     )
     if demographic_image is None:
+        logger.error("No demographic image found for the given age group and sex")
         raise ValueError("No demographic image found for the given age group and sex")
 
     scale = demographic_image.projection().nominalScale().getInfo()
@@ -40,12 +57,12 @@ def get_population_in_zone(
         reducer=Reducer.sum(),
         geometry=zone_vector,
         scale=scale,
-        maxPixels=1e14,
+        maxPixels=1e9,
     ).getInfo()
 
     if result is None:
-        error = "No result found for the given zone vectors"
-        raise ValueError(error)
+        logger.error("No result found for the given zone vectors")
+        raise ValueError("No result found for the given zone vectors")
 
     population_count = result.get(DEMOGRAPHIC_BAND, 0)
     population_millions = round(population_count / 1_000_000, 2)
