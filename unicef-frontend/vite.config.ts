e2f63de009c40bb38ad2369c5f7ca4ecfac54b7e
Federico Bello <fe.debello13@gmail.com>
2025-02-20 16:54:23 -0300

    chore: update frontend stuff

--- unicef-frontend/vite.config.ts
+++ unicef-frontend/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vite/client" />
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-})
+};
