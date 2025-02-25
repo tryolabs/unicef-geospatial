8367d0f1793400af1156f0f5aad6a9b4286529eb
Federico Bello <fe.debello13@gmail.com>
2025-02-25 13:54:44 -0300

    chore: drop all files

--- unicef-frontend/src/types/Message.tsx
+++
@@ -1,8 +0,0 @@
-interface Message {
-  trace_id: string;
-  content: string;
-  role: "user" | "assistant";
-  feedback_given?: number;
-}
-
-export type { Message };
--- unicef-frontend/vite.config.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
-export default {
-  plugins: [react()],
-};
