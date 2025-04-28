69c944848e1f10b989046afd2fb8b7e09d65e724
Federico Bello <fe.debello13@gmail.com>
2025-04-28 15:13:43 -0300

    test: update benchmark

--- tests/test_data.py
+++ tests/test_data.py
@@ -91,88 +91,130 @@ benchmark_questions = []
 
 # DataWarehouse Questions
 
-# benchmark_questions.append(
-#     BechmarkQuestion(
-#         question="What's the percentage of births without a birth weight registered in Nigeria?",
-#         answer=77,
-#         variations=[
-#             "percentage of births without a birth weight registered in Nigeria",
-#         ],
-#     )
-# )
-# benchmark_questions.append(
-#     BechmarkQuestion(
-#         question="What was the percentage of children vaccinated for tuberculosis in Ethiopia in 2020?",
-#         answer=70,
-#         variations=[
-#             "percentage of children vaccinated for tuberculosis in Ethiopia in 2020",
-#         ],
-#     )
-# )
+benchmark_questions.append(
+    BechmarkQuestion(
+        question="What's the percentage of births without a birth weight registered in Nigeria?",
+        answer=77,
+        variations=[
+            "percentage of births without a birth weight registered in Nigeria",
+        ],
+    )
+)
+benchmark_questions.append(
+    BechmarkQuestion(
+        question="What was the percentage of children vaccinated for tuberculosis in Ethiopia in 2020?",
+        answer=70,
+        variations=[
+            "percentage of children vaccinated for tuberculosis in Ethiopia in 2020",
+        ],
+    )
+)
 
 # Multi-Hazard Questions
-# data_single_hazard = {
-#     "river floods": {
-#         "Angola": 714157,
-#         "Nicaragua": 38704,
-#         "Uruguay": 47840,
-#         "Colombia": 797908,
-#     },
-#     "coastal floods": {
-#         "Angola": 6909,
-#         "Nicaragua": 3977,
-#         "Uruguay": 910,
-#         "Colombia": 22714,
-#     },
-#     "pluvial floods": {
-#         "Angola": 8671705,
-#         "Nicaragua": 1178148,
-#         "Uruguay": 613335,
-#         "Colombia": 8158365,
-#     },
-#     "tropical storms": {
-#         "Angola": 0,
-#         "Nicaragua": 2024094,
-#         "Uruguay": 0,
-#         "Colombia": 1748864,
-#     },
-#     "agricultural drought": {
-#         "Angola": 40791,
-#         "Nicaragua": 0,
-#         "Uruguay": 0,
-#         "Colombia": 8077,
-#     },
-#     "fire": {
-#         "Angola": 5394005,
-#         "Nicaragua": 89740,
-#         "Uruguay": 120480,
-#         "Colombia": 157301,
-#     },
-#     "sand and dust storms": {
-#         "Angola": 1027946,
-#         "Nicaragua": 43,
-#         "Uruguay": 88,
-#         "Colombia": 32659,
-#     },
-#     "air pollution": {
-#         "Angola": 16338870,
-#         "Nicaragua": 1976847,
-#         "Uruguay": 604115,
-#         "Colombia": 11212840,
-#     },
-#     "vectorborne malaria pv": {
-#         "Angola": 0,
-#         "Nicaragua": 441785,
-#         "Uruguay": 0,
-#         "Colombia": 6174789,
-#     },
-#     "vectorborne malaria pf": {
-#         "Angola": 15984150,
-#         "Nicaragua": 77840,
-#         "Uruguay": 0,
-#         "Colombia": 366051,
-#     },
-# }
+data_single_hazard = {
+    "agricultural drought": {
+        "Angola": 4099009,
+        "Nicaragua": 878626,
+        "Uruguay": 233589,
+        "Colombia": 3149198,
+    },
+    "air pollution": {
+        "Angola": 16338870,
+        "Nicaragua": 1976847,
+        "Uruguay": 604115,
+        "Colombia": 11212840,
+    },
+    "coastal floods": {
+        "Angola": 6909,
+        "Nicaragua": 3977,
+        "Uruguay": 910,
+        "Colombia": 22714,
+    },
+    "drought SPEI": {
+        "Angola": 573838,
+        "Nicaragua": 6326,
+        "Uruguay": 119691,
+        "Colombia": 2361523,
+    },
+    "drought SPI": {
+        "Angola": 114626,
+        "Nicaragua": 6326,
+        "Uruguay": 115915,
+        "Colombia": 1438632,
+    },
+    "extreme heat": {
+        "Angola": 2360001,
+        "Nicaragua": 574883,
+        "Uruguay": 0,
+        "Colombia": 1272004,
+    },
+    "fire frequency": {
+        "Angola": 5394005,
+        "Nicaragua": 89740,
+        "Uruguay": 120480,
+        "Colombia": 157301,
+    },
+    "fire intensity": {
+        "Angola": 1154385,
+        "Nicaragua": 72040,
+        "Uruguay": 60119,
+        "Colombia": 589614,
+    },
+    "heatwave duration": {
+        "Angola": 13274160,
+        "Nicaragua": 1974811,
+        "Uruguay": 0,
+        "Colombia": 9452636,
+    },
+    "heatwave frequency": {
+        "Angola": 13974860,
+        "Nicaragua": 1971445,
+        "Uruguay": 0,
+        "Colombia": 10066570,
+    },
+    "heatwave severity": {
+        "Angola": 0,
+        "Nicaragua": 0,
+        "Uruguay": 529360,
+        "Colombia": 19277,
+    },
+    "pluvial floods": {
+        "Angola": 8671705,
+        "Nicaragua": 1178148,
+        "Uruguay": 613335,
+        "Colombia": 8158365,
+    },
+    "river floods": {
+        "Angola": 714157,
+        "Nicaragua": 38704,
+        "Uruguay": 47840,
+        "Colombia": 797908,
+    },
+    "sand and dust storms": {
+        "Angola": 1027946,
+        "Nicaragua": 43,
+        "Uruguay": 88,
+        "Colombia": 32659,
+    },
+    "tropical storms": {
+        "Angola": 0,
+        "Nicaragua": 2024094,
+        "Uruguay": 0,
+        "Colombia": 1748864,
+    },
+    "vectorborne malaria pv": {
+        "Angola": 0,
+        "Nicaragua": 441785,
+        "Uruguay": 0,
+        "Colombia": 6174789,
+    },
+    "vectorborne malaria pf": {
+        "Angola": 15984150,
+        "Nicaragua": 77840,
+        "Uruguay": 0,
+        "Colombia": 366051,
+    },
+}
 
 data_multi_hazard = {
     # and river and coastal floods
